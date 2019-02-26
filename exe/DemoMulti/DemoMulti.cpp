581869416c5815d6426276a7f96bd53802dd0b2b
Ada Taylor <adat@andrew.cmu.edu>
2019-02-26 08:49:49 -0500

    Added multiperson classifier capacity to DemoMulti

--- exe/DemoMulti/DemoMulti.cpp
+++ exe/DemoMulti/DemoMulti.cpp
@@ -366,6 +366,7 @@ int main(int argc, char **argv)
 					GastroViz.SetObservationGaze(gaze_direction0, gaze_direction1, LandmarkDetector::CalculateAllEyeLandmarks(face_models[model]), LandmarkDetector::Calculate3DEyeLandmarks(face_models[model], sequence_reader.fx, sequence_reader.fy, sequence_reader.cx, sequence_reader.cy), face_models[model].detection_certainty);
 					GastroViz.SetObservationActionUnits(face_analyser.GetCurrentAUsReg(), face_analyser.GetCurrentAUsClass());
 
+					GastroViz.SetClassifier(model, face_models.size(), LandmarkDetector::GetPose(face_models[model], sequence_reader.fx, sequence_reader.fy, sequence_reader.cx, sequence_reader.cy), face_models[model].detection_certainty, face_analyser.GetCurrentAUsReg(), face_analyser.GetCurrentAUsClass());
 					GastroViz.SetTopView(LandmarkDetector::GetPose(face_models[model], sequence_reader.fx, sequence_reader.fy, sequence_reader.cx, sequence_reader.cy), face_models[model].detection_certainty, face_analyser.GetCurrentAUsReg(), face_analyser.GetCurrentAUsClass());
 
 					// Output features
